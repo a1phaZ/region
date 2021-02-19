e82358907e1531a1fd215998535f7660eab213b4
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2021-02-19 12:18:06 +0500

    + isValid<...>

--- server/helpers/checkInputData.js
+++ server/helpers/checkInputData.js
@@ -1,13 +1,31 @@
+const { Types: {ObjectId}} = require('mongoose');
+
 /***
  * Проверяет существует ли входной объект
  * @param p
  * @returns {boolean}
  */
 const isExist = (p) => {
-	console.log(p, !!p);
 	return !!p
 }
 
+/**
+ * Проверяет валидность id для MongoDB
+ * @param id
+ * @returns {boolean|boolean}
+ */
+const isValidObjectId = (id) => {
+	return (ObjectId.isValid(id) && (new ObjectId(id)).toString() === id)
+}
+
+const isValidEventData = ({title, date, time, timeZone, organizer, maxCount}) => {
+	if (!isExist(title) || !isExist(date) || !isExist(time) || !isExist(timeZone) || !isExist(organizer) || !isExist(maxCount)) return false;
+	// if (!isValidObjectId(organizer)) return false;
+	return true;
+}
+
 module.exports = {
-	isExist
+	isExist,
+	isValidObjectId,
+	isValidEventData
 }
