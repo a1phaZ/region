b864e68c83c3205a17763ff4f77c29acc2a893f2
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2021-02-19 12:20:01 +0500

    add event & group routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const {getGroup, addGroup} = require("./groups");
 const {getUser, addUser} = require("./user");
+const {getEvents, addEvent} = require('./events');
 const router = express.Router();
 
+/**
+ * User route
+ */
 router.get('/user', getUser);
 router.post('/user', addUser);
+
+/**
+ * Group route
+ */
+router.get('/groups/current', getGroup);
+router.post('/groups/current', addGroup);
+
+/**
+ * Events route
+ */
+router.get('/events', getEvents);
+router.post('/events', addEvent);
 
 module.exports = router;
